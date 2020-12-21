cb50e9a7c27585b9df9168806b0d70dd3de67068
kuadra <kuadra@protonmail.com>
2020-12-21 16:48:36 +0100

    not working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Members from './components/Members';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="section-container">
+        <div style={{backgroundColor: "red", height: 100+"px"}}>
+          sei
+        </div>
+      </div>
+      <div className="section-container">
+        <Members />
+      </div>
     </div>
   );
 }
