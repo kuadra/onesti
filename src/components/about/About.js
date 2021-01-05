c48127f34a407f3df62e4389c7d23103df4ae386
kuadra <kuadra@protonmail.com>
2021-01-05 13:35:21 +0100

    examples list inline block

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,24 @@
 import React, { Component } from "react";
 import "./about.css";
+
+const esempi = [
+  "Strategie Push di Vendita per il Mercato Online",
+  "Brand Identity, Logo e Comunicazione",
+  "Creazione e Gestione Campagne Pubblicitarie e di Marketing ad Alto Impatto",
+  "Gestione Social Network e contenuti (FB, Instagram, Youtube, LinkedIn, TikTok, Pintrest)",
+  "Search Marketing Organico e a Pagamento (SEO-SEM) su ogni Motore di Ricerca (Google, Yahoo, Bing) e App. web (Google Maps, Waze)",
+  "Creazione Siti Web, E-commerce e App.",
+  "Gestione Siti Web: contenuti e strategia",
+  "Gestione E-Commerce: tecnica online e logistica/strategica offline",
+  "Video: Creazione Adv Video e Contenuti Video",
+  "Consulenze Gestioni IN-store",
+  "Creazione Nuove Opportunità: espansione su altri mercati o estero tramite digitale.",
+];
+
 class About extends Component {
   render() {
+    let e = esempi.map((p) => <li className="item inline-b-item">{p}</li>);
+
     return (
       <section id="about">
         <div className="section-img">
@@ -80,7 +97,10 @@ class About extends Component {
                 className="section-img"
                 src="./images/onesti/adv_personalizzata.jpg"
               ></img>
-              <h3>Creazione advertising personalizzata e gestione campagne pubblicitarie</h3>
+              <h3>
+                Creazione advertising personalizzata e gestione campagne
+                pubblicitarie
+              </h3>
               <p>
                 La nostra expertise ci permette di offrire ogni tipo di
                 creatività pubblicitaria dalle grafiche ai video. Grafiche in
@@ -93,6 +113,16 @@ class About extends Component {
               </p>
             </div>
           </div>
+        </div>
+        <div className="row">
+          <h5>Alcuni Esempi</h5>
+          <ul className="container inline-b">{e}</ul>
+          <p className="section-intro">
+            Siamo in grado di offrire una gestione multicanale che ci rende
+            sicuramente i migliori partner per la tua azienda, Mettici alla
+            prova e non te ne pentirai. Tanti servizi, tante competenze, un
+            approccio umano e soprattutto ONESTO! Insieme per il successo!
+          </p>
         </div>
       </section>
     );
